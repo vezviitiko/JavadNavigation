72bdad9784e4f860f128b033bd943d74e6cdde33
vezviitiko <38458279+vezviitiko@users.noreply.github.com>
2018-05-10 14:37:58 +0300

    Update model.h
    Исправлено и вынесено все в функции
    Изменение от 14:36 от 10.05

--- model.h
+++ model.h
@@ -1,33 +1,10 @@
 #ifndef _model_model_h
 #define _model_model_h
 #include <iostream>
+#include <string>
 #include <Core/Core.h>
+#include "CoordSys.h"
 
 using namespace Upp;
-/*
-public class matrix_multiplication
-{
-	int matr[][] = new int[N][N];
-	int vect[]   = new int[N];
-	
-	// инициализация
-	for(int i = 0; i< N;i++)
-	{
-		vect[i] = rand() % 10;
-	}
-	
-	for (i = 0; i < N; i++)
-    	for (j = 0; j < N; j++)
-    	{
-    		B[i][j] = rand() % 10;
-    	}
-    	
-    	
-    	
-	void iuty();
-}*/
 
 #endif
-
-
-
