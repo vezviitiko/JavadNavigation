3bc6f24b044728b43ae851970cd6fff8bfda9f40
vezviitiko <38458279+vezviitiko@users.noreply.github.com>
2018-05-15 12:44:23 +0300

    Update model.h

--- model.h
+++ model.h
@@ -1,31 +1,24 @@
 #ifndef _model_model_h
 #define _model_model_h
 #include <iostream>
+#include <string>
 #include <Core/Core.h>
 
+#include "CoordSys.h"
+#include "DataConverter.h"
+
 using namespace Upp;
-/*
-public class matrix_multiplication
-{
-	int matr[][] = new int[N][N];
-	int vect[]   = new int[N];
-	
-	// инициализация
-	for(int i = 0; i< N;i++)
-	{
-		vect[i] = rand() % 10;
-	}
-	
-	for (i = 0; i < N; i++)
-    	for (j = 0; j < N; j++)
-    	{
-    		B[i][j] = rand() % 10;
-    	}
-    	
-    	
-    	
-	void iuty();
-}*/
+
+const double PI = 3.1415926535897932384626433832795;
+
+float dateTojd(int year,int month, int day);
+float jdTomjd(float jd);
+float mjdTojd(float mjd);
+float datetimeTojd(CDatetime);
+CDate jdTodate(float jd);
+CDatetime jdTodatetime(float jd);
+float hmsmTodays(int hour, int min,int sec, int micro);
+CDatetime daysTohmsm(float days);
 
 #endif
 
