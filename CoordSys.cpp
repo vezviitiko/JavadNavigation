0b2f912b111e89c1f9144b34b4ee98c40820e5bb
vezviitiko <38458279+vezviitiko@users.noreply.github.com>
2018-06-05 12:48:01 +0300

    Добавлены функции RadEarthLat и DegreesToRad
    Добавлены функции RadEarthLat и DegreesToRad

--- CoordSys.cpp
+++ CoordSys.cpp
@@ -1,5 +1,6 @@
 #include <Core/Core.h>
 #include "CoordSys.h"
+#include "kamaz.h"
 
 using namespace Upp;
 
@@ -27,4 +28,23 @@ void CKeplerian::trueAnomCalc()
 	double at2Cos = ((cos(E)- e)/(1.0 - e*cos(E)));
 	
 	v = atan2(at2Sin, at2Cos);
+}
+
+double RadEarthLat(double lat)
+{
+	double a = 6378137.0;    // BigSemiaxis м
+	double b = 6356752.3142; // SmallSemiaxis м
+	
+	// перевод градусов в радианы
+	lat = DegreesToRad(lat);
+	// геодезической широты перейти к геоцентрической широте
+	lat = (a/b)*tan(lat);
+
+	return sqrt((pow(a,4.) + pow(b,4.)*pow(lat,2.)) / (pow(a,2.) + pow(b,2.)*pow(lat,2.)));
+}
+
+// Функция перевода градусов в радианы
+double DegreesToRad(double in)
+{
+	return in*PI/180.;
 }
