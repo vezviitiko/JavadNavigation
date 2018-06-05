cec412836b5c466d5f6706addb84b03b1d353895
Paul <esipov.p@mail.ru>
2018-06-05 15:54:30 +0300

    Revert "Normal"
    This reverts commit 4c4bd7905ca433d7fa8b8de7da75e132012dae15.

--- Almanac.cpp
+++ Almanac.cpp
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 #include "model.h"
 //#include "cmath"
-=======
-#include "kamaz.h"
->>>>>>> pr/51
 
 CCartesian ModelAlmDec(CAlmanac alm)
 {
@@ -15,7 +11,6 @@ CCartesian ModelAlmDec(CAlmanac alm)
 	double i = alm.icp+alm.di;
 	double Tdr = alm.Tcp + alm.dT;
 	double p, Tock;
-<<<<<<< HEAD
 	
 	double a = pow((pow((Tdr/(2*PI)),2)*alm.ny),1/3);
 	
@@ -33,24 +28,6 @@ CCartesian ModelAlmDec(CAlmanac alm)
 	//2. Рассчёт момента прохождения восходящего узла орбиты на витке,
 	// к которому принадлежит  момент, и долготы восходящего узла на этом витке
 	
-=======
-	double a = (double) pow((double) pow((Tdr/(2.*PI)),2.)*alm.ny,1./3.);
-	RDUMP(a);
-	double a1;
-	for (int ix = 0; ix < 3; ix++){
-		
-		p = a*(1.0-pow(alm.e,2.));
-		a1 = a;
-		Tock = Tdr * ( 1.+(3./2.)*alm.C*pow((alm.ae/p),2.)*((2.-(5./2.)*pow(sin(i),2.))*
-		(pow((1.-pow(alm.e,2.)),(3./2.))/pow((1+alm.e*cos(alm.w)),2.))
-		+ (pow((1.+alm.e*cos(v)),3.)/(1.-pow(alm.e,2.))) ) );
-		
-		a = pow(pow((Tock/(2.*PI)),2.)*alm.ny,(1./3.));
-	}	// реализовано вместо mod(a(3)-a(2))<pow(10,-3) == for (int ix = 0; ix < 3; ix++)
-		
-	//2. Рассчёт момента прохождения восходящего узла орбиты на витке,
-	// к которому принадлежит момент, и долготы восходящего узла на этом витке	
->>>>>>> pr/51
 	// вычисление ближайшего високосного года
 	int year = alm.year;
 
@@ -59,7 +36,6 @@ CCartesian ModelAlmDec(CAlmanac alm)
 	}
 	RDUMP(year);
 	// календарный номер суток внутри четырехлетнего периода от начала ближайшего високосного года
-<<<<<<< HEAD
 	
 	float Na = Date(alm.yearAlm,alm.monthAlm,alm.dayAlm) - Date(year,1,1);
 	RDUMP(Na);
@@ -77,38 +53,11 @@ CCartesian ModelAlmDec(CAlmanac alm)
 	int W   = (int) Wk;
 	RDUMP(W);
 	double tlk = alm.Tlambda + Tdr * W + alm.dTT * pow(W,2);
-=======
-	// по альманаху
-	float Na = Date(alm.yearAlm,alm.monthAlm,alm.dayAlm) - Date(year,1,1);
-	RDUMP(Na);
-	// по данным
-	float N0 = Date(alm.year,alm.month,alm.day) - Date(year,1,1);
-	RDUMP(N0);
-	
-	/* истинное звёздное время - используется среднне 
-	http://www.astrolib.ru/files/6/Duboshin-Sprav_Ruk_po_Neb_Meh.pdf
-	стр 159
-	*/
-	double t = dateTojd(alm.year,alm.month,alm.day);
-	double d = t - 2415020.;
-	RDUMP(d);
-	float S0 = 0.276919398 + 0.00273790926493*d+0.1075231*pow(10.,-5.)*(d/36525.);
-	RDUMP(S0);
-	
-	double tStar = t - alm.Tlambda + 86400.*(N0-Na);
-	RDUMP(tStar);
-	double Wk = tStar/Tdr;
-	RDUMP(Wk);
-	int W   = (int) Wk;
-	RDUMP(W);
-	double tlk = alm.Tlambda + Tdr * W + alm.dTT * pow(W,2.0);
->>>>>>> pr/51
 	RDUMP(tlk);
 	tlk = (int) tlk%86400; // mod
 	RDUMP(tlk);
 	// =====================
 	
-<<<<<<< HEAD
 	double n = (2*PI)/Tdr;
 	
 	double omBigHatch = (3/2)*alm.C*n*pow((alm.ae/a),2)*cos(i)*pow((1-pow(alm.e,2)),-2);
@@ -118,40 +67,20 @@ CCartesian ModelAlmDec(CAlmanac alm)
 	double S = S0 + alm.omEarth*(tlk-10800);
 	
 	double psi = lk + S;
-=======
-	double n = (2.*PI)/Tdr;
-	
-	double omBigHatch = (3./2.)*alm.C*n*pow((alm.ae/a),2.)*cos(i)*pow((1.-pow(alm.e,2.)),-2.);
-	
-	double lk = alm.lambda + (omBigHatch - alm.omEarth)*(Tdr*W+alm.dTT*pow(W,2.));
-	
-	double S = S0 + alm.omEarth*(tlk-10800.);
-	
-	double omBig = lk + S;
->>>>>>> pr/51
 	
 	
 	// 3. Вычисление констант интегрирования
 	
-<<<<<<< HEAD
 	double J = (-3/2)*alm.C;
 	double l = alm.e*cos(alm.w);
 	double h = alm.e*sin(alm.w);
 
 	double E = atan(sqrt((1-alm.e)/(1+alm.e))*tan(v/2))/2;
-=======
-	double J = (-3./2.)*alm.C;
-	double l = alm.e*cos(alm.w);
-	double h = alm.e*sin(alm.w);
-
-	double E = 2.*atan(sqrt((1.-alm.e)/(1.+alm.e))*tan(v/2.));
->>>>>>> pr/51
 	double M = E - alm.e*sin(E);
 	
 	double lambdaHatch = M + alm.w;
 	
 	// ======================================================
-<<<<<<< HEAD
 	double m = 1;
 	double ro = 0;
 	
@@ -166,52 +95,12 @@ CCartesian ModelAlmDec(CAlmanac alm)
 	double di_1 = (1/2)*J*pow((alm.ae/a),2)*sin(i)*cos(i)*(-l*cos(lambdaHatch)+h*sin(lambdaHatch)+cos(2*lambdaHatch)+(7/3)*l*cos(3*lambdaHatch)+(7/3)*h*sin(3*lambdaHatch));
 	
 	double dlambda_1 = 2*J*pow((alm.ae/a),2)*(1-(3/2)*pow((sin(i)),2))*(n*ro+(7/4)*l*sin(lambdaHatch)-(7/4)*h*cos(2*lambdaHatch))+3*J*pow((alm.ae/a),2)*pow((sin(i)),2)*((-7/24)*h*cos(lambdaHatch)-(7/24)*l*sin(lambdaHatch)-(49/72)*l*sin(3*lambdaHatch)+(1/4)*sin(2*lambdaHatch))+J*pow((alm.ae/a),2)*pow(cos(i),2)*(n*ro+(7/2)*l*sin(lambdaHatch)-(5/2)*h*cos(lambdaHatch)-(1/2)*sin(2*lambdaHatch)-(7/6)*l*sin(3*lambdaHatch)+(7/6)*h*cos(3*lambdaHatch));
-=======
-	double m = 1.;
-	double ro = 0.;
-	
-	double da_1 = (2.*J*pow((alm.ae/a),2.)*(1.-(3./2.)*pow((sin(i)),2.))*
-				(alm.lambda*cos(lambdaHatch) + h*sin(lambdaHatch)) + J*pow((alm.ae/a),2.)*
-				pow(sin(i),2.)*(0.5*h*sin(lambdaHatch) -
-				0.5*l*cos(lambdaHatch)+cos(2*lambdaHatch) +
-				(7./2.)*l*cos(3.*lambdaHatch)+(7./2.)*h*sin(3.*lambdaHatch)))/a;
-	
-	double dh_1 = J*pow((alm.ae/a),2.)*(1.-(3./2.)*pow((sin(i)),2.))*
-				( l*n*ro+sin(lambdaHatch)+(3./2.)*l*sin(2.*lambdaHatch)-(3./2.)*h*cos(2.*lambdaHatch)) -
-				(1./4.)*J*pow((alm.ae/a),2.)*pow(sin(i),2.)*( sin(lambdaHatch)-(7./3.)*sin(3.*lambdaHatch) +
-				5.*l*sin(2.*lambdaHatch)-(17./2.)*l*sin(4.*lambdaHatch)+(17./2.)*h*cos(4.*lambdaHatch) +
-				h*cos(2.*lambdaHatch)) + J*pow((alm.ae/a),2.)*
-				pow(cos(i),2.) * ( l*n*ro-(1./2.)*l*sin(2.*lambdaHatch));
-	
-	double dl_1 = J*pow((alm.ae/a),2.)*(1.-(3./2.)*pow((sin(i)),2.))*
-				(-h*n*ro+cos(lambdaHatch)+(3./2.)*l*cos(2.*lambdaHatch)+(3./2.)*h*sin(2.*lambdaHatch)) -
-				(1./4.)*J*pow((alm.ae/a),2.)*pow(sin(i),2.)*(-cos(lambdaHatch)-(7./3.)*cos(3.*lambdaHatch) -
-				5.*h*sin(2.*lambdaHatch)-(17./2.)*l*cos(4.*lambdaHatch)-(17./2.)*h*sin(4.*lambdaHatch) +
-				l*cos(2.*lambdaHatch)) + J*pow((alm.ae/a),2.)*pow(cos(i),2.) *
-				(-h*n*ro+(1./2.)*h*sin(2.*lambdaHatch));
-	
-	double domBig_1 = -J*pow((alm.ae/a),2.)*cos(i)*(n*ro+(7./2.)*l*sin(lambdaHatch) -
-				(5./2.)*h*cos(lambdaHatch) - (1./2.)*sin(2.*lambdaHatch) -
-				(7./6.)*l*sin(3.*lambdaHatch) + (7./6.)*h*cos(3.*lambdaHatch));
-	
-	double di_1 = (1./2.)*J*pow((alm.ae/a),2.)*sin(i)*cos(i)*(-l*cos(lambdaHatch) +
-				h*sin(lambdaHatch) + cos(2.*lambdaHatch) +
-				(7./3.)*l*cos(3.*lambdaHatch) + (7./3.)*h*sin(3.*lambdaHatch));
-	
-	double dlambda_1 = 2.*J*pow((alm.ae/a),2.)*(1.-(3./2.)*pow((sin(i)),2.))*(n*ro+(7./4.)*l*sin(lambdaHatch) -
-				(7./4.)*h*cos(2.*lambdaHatch)) + 3.*J*pow((alm.ae/a),2.)*pow((sin(i)),2.)*
-				((-7./24.)*h*cos(lambdaHatch) - (7./24.)*l*sin(lambdaHatch) -
-				(49./72.)*h*cos(3.*lambdaHatch) + (49./72.)*l*sin(3.*lambdaHatch) + (1./4.)*sin(2.*lambdaHatch)) +
-				J*pow((alm.ae/a),2.)*pow(cos(i),2.)*(n*ro+(7./2.)*l*sin(lambdaHatch)-(5./2.)*h*cos(lambdaHatch) -
-				(1./2.)*sin(2.*lambdaHatch)-(7./6.)*l*sin(3.*lambdaHatch)+(7./6.)*h*cos(3.*lambdaHatch));
->>>>>>> pr/51
 	
 	// ======================================================
 	m = 2;
 	ro = t - tlk;
 	lambdaHatch = M + alm.w + n*ro;
 	
-<<<<<<< HEAD
 	double da_2 = (2*J*pow((alm.ae/a),2)*(1-(3/2)*pow((sin(i)),2))*(alm.lambda*cos(lambdaHatch)+h*sin(lambdaHatch))+J*pow((alm.ae/a),2)*pow(sin(i),2)*(0.5*h*sin(lambdaHatch)-0.5*l*cos(lambdaHatch)+cos(2*lambdaHatch)+(7/2)*l*cos(3*lambdaHatch)+(7/2)*h*sin(3*lambdaHatch)))/a;
 	
 	double dh_2 = J*pow((alm.ae/a),2)*(1-(3/2)*pow((sin(i)),2))*( l*n*ro+sin(lambdaHatch)+(3/2)*l*sin(2*lambdaHatch)-(3/2)*h*cos(2*lambdaHatch)) - (1/4)*J*pow((alm.ae/a),2)*pow(sin(i),2)*( sin(lambdaHatch)-(7/3)*sin(3*lambdaHatch)+5*l*sin(2*lambdaHatch)-(17/2)*l*sin(4*lambdaHatch)+(17/2)*h*cos(4*lambdaHatch)+h*cos(2*lambdaHatch))+J*pow((alm.ae/a),2)*pow(cos(i),2)*( l*n*ro-(1/2)*l*sin(2*lambdaHatch));
@@ -224,42 +113,6 @@ CCartesian ModelAlmDec(CAlmanac alm)
 	
 	double dlambda_2 = 2*J*pow((alm.ae/a),2)*(1-(3/2)*pow((sin(i)),2))*(n*ro+(7/4)*l*sin(lambdaHatch)-(7/4)*h*cos(2*lambdaHatch))+3*J*pow((alm.ae/a),2)*pow((sin(i)),2)*((-7/24)*h*cos(lambdaHatch)-(7/24)*l*sin(lambdaHatch)-(49/72)*l*sin(3*lambdaHatch)+(1/4)*sin(2*lambdaHatch))+J*pow((alm.ae/a),2)*pow(cos(i),2)*(n*ro+(7/2)*l*sin(lambdaHatch)-(5/2)*h*cos(lambdaHatch)-(1/2)*sin(2*lambdaHatch)-(7/6)*l*sin(3*lambdaHatch)+(7/6)*h*cos(3*lambdaHatch));
 	
-=======
-	double da_2 = (2.*J*pow((alm.ae/a),2.)*(1.-(3./2.)*pow((sin(i)),2.))*
-				(alm.lambda*cos(lambdaHatch) + h*sin(lambdaHatch)) + J*pow((alm.ae/a),2.)*
-				pow(sin(i),2.)*(0.5*h*sin(lambdaHatch) -
-				0.5*l*cos(lambdaHatch)+cos(2*lambdaHatch) +
-				(7./2.)*l*cos(3.*lambdaHatch)+(7./2.)*h*sin(3.*lambdaHatch)))/a;
-	
-	double dh_2 = J*pow((alm.ae/a),2.)*(1.-(3./2.)*pow((sin(i)),2.))*
-				( l*n*ro+sin(lambdaHatch)+(3./2.)*l*sin(2.*lambdaHatch)-(3./2.)*h*cos(2.*lambdaHatch)) -
-				(1./4.)*J*pow((alm.ae/a),2.)*pow(sin(i),2.)*( sin(lambdaHatch)-(7./3.)*sin(3.*lambdaHatch) +
-				5.*l*sin(2.*lambdaHatch)-(17./2.)*l*sin(4.*lambdaHatch)+(17./2.)*h*cos(4.*lambdaHatch) +
-				h*cos(2.*lambdaHatch)) + J*pow((alm.ae/a),2.)*
-				pow(cos(i),2.) * ( l*n*ro-(1./2.)*l*sin(2.*lambdaHatch));
-	
-	double dl_2 = J*pow((alm.ae/a),2.)*(1.-(3./2.)*pow((sin(i)),2.))*
-				(-h*n*ro+cos(lambdaHatch)+(3./2.)*l*cos(2.*lambdaHatch)+(3./2.)*h*sin(2.*lambdaHatch)) -
-				(1./4.)*J*pow((alm.ae/a),2.)*pow(sin(i),2.)*(-cos(lambdaHatch)-(7./3.)*cos(3.*lambdaHatch) -
-				5.*h*sin(2.*lambdaHatch)-(17./2.)*l*cos(4.*lambdaHatch)-(17./2.)*h*sin(4.*lambdaHatch) +
-				l*cos(2.*lambdaHatch)) + J*pow((alm.ae/a),2.)*pow(cos(i),2.) *
-				(-h*n*ro+(1./2.)*h*sin(2.*lambdaHatch));
-	
-	double domBig_2 = -J*pow((alm.ae/a),2.)*cos(i)*(n*ro+(7./2.)*l*sin(lambdaHatch) -
-				(5./2.)*h*cos(lambdaHatch) - (1./2.)*sin(2.*lambdaHatch) -
-				(7./6.)*l*sin(3.*lambdaHatch) + (7./6.)*h*cos(3.*lambdaHatch));
-	
-	double di_2 = (1./2.)*J*pow((alm.ae/a),2.)*sin(i)*cos(i)*(-l*cos(lambdaHatch) +
-				h*sin(lambdaHatch) + cos(2.*lambdaHatch) +
-				(7./3.)*l*cos(3.*lambdaHatch) + (7./3.)*h*sin(3.*lambdaHatch));
-	
-	double dlambda_2 = 2.*J*pow((alm.ae/a),2.)*(1.-(3./2.)*pow((sin(i)),2.))*(n*ro+(7./4.)*l*sin(lambdaHatch) -
-				(7./4.)*h*cos(2.*lambdaHatch)) + 3.*J*pow((alm.ae/a),2.)*pow((sin(i)),2.)*
-				((-7./24.)*h*cos(lambdaHatch) - (7./24.)*l*sin(lambdaHatch) -
-				(49./72.)*h*cos(3.*lambdaHatch) + (49./72.)*l*sin(3.*lambdaHatch) + (1./4.)*sin(2.*lambdaHatch)) +
-				J*pow((alm.ae/a),2.)*pow(cos(i),2.)*(n*ro+(7./2.)*l*sin(lambdaHatch)-(5./2.)*h*cos(lambdaHatch) -
-				(1./2.)*sin(2.*lambdaHatch)-(7./6.)*l*sin(3.*lambdaHatch)+(7./6.)*h*cos(3.*lambdaHatch));
->>>>>>> pr/51
 	
 	//4. Вычисление поправок к элементам орбиты НКА
 	
@@ -276,7 +129,6 @@ CCartesian ModelAlmDec(CAlmanac alm)
 	l = l + dl;
 	
 	double eps = sqrt(sqr(h)+sqr(l));
-<<<<<<< HEAD
 	double omBig;
 	
 	if ((eps != 0) && (eps != 0))
@@ -287,28 +139,12 @@ CCartesian ModelAlmDec(CAlmanac alm)
 				omBig = PI/2;
 			else if ((eps != 0) && (h == -eps))
 				omBig = -PI/2;
-=======
-	double omMIN;
-	
-	if ((eps != 0) && (eps != 0))
-		omMIN = atan(h/l);
-	else if (eps == 0)
-			omMIN = 0;
-		else if ((eps != 0) && (h == eps))
-				omMIN = PI/2.;
-			else if ((eps != 0) && (h == -eps))
-				omMIN = -PI/2.;
->>>>>>> pr/51
 			
 	a = a + da;
 	i = i + di;
 	omBig = omBig + domBig;
 	double lStar = M + alm.w + n*(t-tlk)+dlambda;
-<<<<<<< HEAD
 	M = lStar - alm.w;
-=======
-	M = lStar - omMIN;
->>>>>>> pr/51
 	
 	//6. Вычисление координат
 	CKeplerian kepTest;
@@ -320,11 +156,7 @@ CCartesian ModelAlmDec(CAlmanac alm)
 
 	E = kepTest.E;
 	
-<<<<<<< HEAD
 	v = atan(sqrt((1-eps)/(1+eps))*tan(E/2))/2;
-=======
-	v = 2.*atan(sqrt((1.-eps)/(1.+eps))*tan(E/2.));
->>>>>>> pr/51
 		
 	double u = v + alm.w;
 	
--- CoordFun.cpp
+++ CoordFun.cpp
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 #include "model.h"
-=======
-#include "kamaz.h"
->>>>>>> pr/51
 
 CCartesian KepToDec(CKeplerian kep)
 {
@@ -47,15 +43,9 @@ CCartesian KepToDec(CKeplerian kep)
 CAnglesRot RotVector(CAnglesRot ang)
 {
 	// матрица поворота
-<<<<<<< HEAD
 	double **arrRotMat = new double* [3]; 
     	for (int count = 0; count < 3; count++)
     		arrRotMat[count] = new double [3];
-=======
-	double **arrRotMat = new double* [3];
-		for (int count = 0; count < 3; count++)
-			arrRotMat[count] = new double [3];
->>>>>>> pr/51
 
 	arrRotMat[0][0] = cos(ang.fi)*cos(ang.psi)-sin(ang.fi)*sin(ang.psi)*cos(ang.teta);
 	arrRotMat[1][0] = -sin(ang.fi)*cos(ang.psi)-cos(ang.fi)*sin(ang.psi)*cos(ang.teta);
@@ -101,31 +91,9 @@ CAnglesRot RotVector(CAnglesRot ang)
 	for (int count = 0; count < 3; count++)
        delete [] arrRotMat[count];
 	delete [] arrRotMat;
-<<<<<<< HEAD
 	   
 	RDUMP(ang.x);
 	RDUMP(ang.y);
 	RDUMP(ang.z);
 	return ang;
 }
-=======
-
-	return ang;
-}
-
-CCartesian SphCoordToDec(CSpherical sph)
-{
-	CCartesian car;
-	// перевод в геоцентрическую не инерциальную
-	
-	car.z = sph.radEarth*cos(sph.lat);
-	car.y = sph.radEarth*sin(sph.lat);
-	car.x = sph.radEarth*sin(sph.lon);
-	
-	RDUMP("========================");
-	RDUMP(car.x);
-	RDUMP(car.y);
-	RDUMP(car.z);
-	return car;
-}
->>>>>>> pr/51
