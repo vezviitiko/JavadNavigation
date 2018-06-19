59d930484aa9932ebb06960da4b6668f65bcb062
EsipovPA <38457822+EsipovPA@users.noreply.github.com>
2018-06-19 12:24:18 +0300

    Add files via upload

--- JpsFiles.cpp
+++ JpsFiles.cpp
@@ -40,7 +40,6 @@ void JpsFile::readFile()
 	fileLen = inFileStr.tellg();
 	inFileStr.seekg(0, inFileStr.beg);
 	
-	
 	cout << "test output\n";
 	for (int i = 0; i < fileLen; i++){
 		inFileStr.read((char*)&buf,1);
@@ -54,88 +53,372 @@ void JpsFile::readFile()
 			test = ""; test += str[0]; test += str[1];
 //			if (test == "JP") {LOG("Have found JP");}
 //			if (test == "MF") {LOG("Have found MF");}
-			if (test == "~~") {JpsData* jps = new ReceiverTime("RT", strLen, str); jpsData.Add(jps); delete jps;}
-//			if (test == "::") {	LOG("Have found ET");}
-			if (test == "RD") {JpsData* jps = new ReceiverDate("RD", strLen, str); jpsData.Add(jps); delete jps;}
-			if (test == "TO") {
-				JpsData* jps = new RTtoRTO("TO", strLen, str);
-				jpsData.Add(jps);
-				delete jps;
+			if (test == "~~") {
+				jpsData.Add(new ReceiverTime("RT", strLen, str));
 				}
-//			if (test == "DO") {LOG("Have found DO");}
-//			if (test == "BP") {LOG("Have found BP");}
-//			if (test == "GT") {LOG("Have found GT");}
-//			if (test == "GO") {LOG("Have found GO");}
-//			if (test == "NT") {LOG("Have found NT");}
-//			if (test == "NO") {LOG("Have found NO");}
-//			if (test == "EO") {LOG("Have found EO");}
-//			if (test == "WO") {LOG("Have found WO");}
-//			if (test == "QO") {LOG("Have found QO");}
-//			if (test == "CO") {LOG("Have found CO");}
-//			if (test == "Io") {LOG("Have found Io");}
-//			if (test == "UO") {LOG("Have found UO");}
-//			if (test == "WU") {LOG("Have found WU");}
-//			if (test == "EU") {LOG("Have found EU");}
-//			if (test == "QU") {LOG("Have found QU");}
-//			if (test == "CU") {LOG("Have found CU");}
-//			if (test == "IU") {LOG("Have found IU");}
-//			if (test == "NU") {LOG("Have found NU");}
-//			if (test == "ST") {LOG("Have found ST");}
-//			if (test == "PO") {LOG("Have found PO");}
-//			if (test == "VE") {LOG("Have found VE");}
-//			if (test == "PV") {LOG("Have found PV");}
-//			if (test == "PG") {LOG("Have found PG");}
-//			if (test == "VG") {LOG("Have found VG");}
-//			if (test == "SG") {LOG("Have found SG");}
-//			if (test == "mp") {LOG("Have found mp");}
-//			if (test == "bp") {LOG("Have found bp");}
-//			if (test == "DP") {LOG("Have found DP");}
-//			if (test == "SP") {LOG("Have found SP");}
-//			if (test == "SV") {LOG("Have found SV");}
-//			if (test == "BL") {LOG("Have found BL");}
-//			if (test == "bL") {LOG("Have found bL");}
-//			if (test == "mR") {LOG("Have found mR");}
-//			if (test == "PS") {LOG("Have found PS");}
-//			if (test == "SI") {LOG("Have found SI");}
-//			if (test == "AN") {LOG("Have found AN");}
-//			if (test == "NN") {LOG("Have found NN");}
-			if (test == "EL") {
-				JpsData* jps = new SatElevations("EL", strLen, str, 1);
-				jpsData.Add(jps);
-				delete jps;
+				
+			if (test == "::") {
+				LOG("Have found ET");
 			}
-//			if (test == "AZ") {LOG("Have found AZ");}
-//			if (test == "RC") {LOG("Have found RC");}
-//			if (test == "R1") {LOG("Have found R1");}
-//			if (test == "R2") {LOG("Have found R2");}
-//			if (test == "R3") {LOG("Have found R3");}
-//			if (test == "R5") {LOG("Have found R5");}
-//			if (test == "Rl") {LOG("Have found RI");}
-//			if (test == "rx") {LOG("Have found rx");}
-//			if (test == "rc") {LOG("Have found rc");}
-//			if (test == "r1") {LOG("Have found r1");}
-//			if (test == "r2") {LOG("Have found r2");}
-//			if (test == "r3") {LOG("Have found r3");}
-//			if (test == "r5") {LOG("Have found r5");}
-//			if (test == "rl") {LOG("Have found rl");}
-//			if (test == "1R") {LOG("Have found 1R");}
-//			if (test == "2R") {LOG("Have found 2R");}
-//			if (test == "3R") {LOG("Have found 3R");}
-//			if (test == "5R") {LOG("Have found 5R");}
-//			if (test == "lR") {LOG("Have found lR");}
-//			if (test == "cr") {LOG("Have found cr");}
-//			if (test == "1r") {LOG("Have found 1r");}
-//			if (test == "2r") {LOG("Have found 2r");}
-//			if (test == "3r") {LOG("Have found 3r");}
-//			if (test == "5r") {LOG("Have found 5r");}
-//			if (test == "lr") {LOG("Have found lr");}
-//			if (test == "GT") {LOG("Have found GP");}
+			if (test == "RD") {
+				jpsData.Add(new ReceiverDate("RD", strLen, str));
+			}
+			
+			if (test == "TO") {
+				jpsData.Add(new RTtoRTO("TO", strLen, str));
+				}
+			if (test == "DO") {
+				jpsData.Add(new DerOfRtOffset("DO", strLen, str));
+			}
+			if (test == "BP") {
+				jpsData.Add(new RoughAccurOfTimApprox("BP", strLen, str));
+			}
+			if (test == "GT") {
+				jpsData.Add(new GpsTime("GT", strLen, str));
+			}
+			if (test == "GO") {
+				jpsData.Add(new GpsToRecTimOff("GO", strLen, str));
+			}
+			if (test == "NT") {
+				jpsData.Add(new GloTime("NT", strLen, str));
+			}
+			if (test == "NO") {
+				jpsData.Add(new GloRecTimOff("NO", strLen, str));
+			}
+			if (test == "EO") {
+				jpsData.Add(new GalRecTimOff("EO", strLen, str));
+			}
+			if (test == "WO") {
+				jpsData.Add(new SbasRecTimOff("WO", strLen, str));
+			}
+			if (test == "QO") {
+				jpsData.Add(new QzssRecTimOff("QO", strLen, str));
+			}
+			if (test == "CO") {
+				jpsData.Add(new BeiRecTimOff("CO", strLen, str));
+			}
+			if (test == "Io") {
+				jpsData.Add(new IrnssRecTimOff("Io", strLen, str));
+			}
+			if (test == "UO") {
+				jpsData.Add(new GpsUtcTimParam("UO", strLen, str));
+			}
+			if (test == "WU"){
+				jpsData.Add(new SBASUtcTimParam("WU", strLen, str));
+			}
+			if (test == "EU"){
+				jpsData.Add(new GalUtcGpsParam("EU", strLen, str));
+			}
+//			if (test == "QU") {
+//				jpsData.Add(new QzssUtcParam("QU", strLen, str));
+//			}
+//			if (test == "CU") {
+//				jpsData.Add(new BeiDouUtcParam("CU", strLen, str));
+//			}
+//			if (test == "IU") {
+//				jpsData.Add(new IrnssUtcParam("IU", strLen, str));
+//			}
+			if (test == "NU") {
+				jpsData.Add(new GloUtcGpsParam("NU", strLen, str));
+			}
+			if (test == "ST") {
+				jpsData.Add(new SolutionTime("ST", strLen, str));
+			}
+			if (test == "PO") {
+				jpsData.Add(new Pos("PO", strLen, str));
+			}
+			if (test == "VE") {
+				jpsData.Add(new Vel("VE", strLen, str));
+			}
+			if (test == "PV") {
+				jpsData.Add(new PosVel("PV", strLen, str));
+			}
+			if (test == "PG") {
+				jpsData.Add(new GeoPos("PG", strLen, str));
+			}
+			if (test == "VG") {
+				jpsData.Add(new GeoVel("VG", strLen, str));
+			}
+			if (test == "SG") {
+				jpsData.Add(new Rms("SG", strLen, str));
+			}
+			if (test == "mp") {
+				jpsData.Add(new LocalPlanePos("mp", strLen, str));
+			}
+//			if (test == "bp") {
+//				jpsData.Add(new RSLocalPlanePos("bp", strLen, str));
+//			}
+			if (test == "DP")  {
+				jpsData.Add(new Dops("DP", strLen, str));
+			}
+			if (test == "SP")   {
+				jpsData.Add(new PosCov("SP", strLen, str));
+			}
+//			if (test == "SV")   {
+//				jpsData.Add(new VelCov("SV", strLen, str));
+//			}
+			if (test == "BL")  {
+				jpsData.Add(new Baseline("BL", strLen, str));
+			}
+			if (test == "bL")  {
+				jpsData.Add(new Baselines("bL", strLen, str));
+			}
+			if (test == "mR") {
+				jpsData.Add(new FullRotationMatr("mR", strLen, str));
+			}
+			if (test == "PS")  {
+				jpsData.Add(new PosStat("PS", strLen, str));
+			}
+			if (test == "PT")  {
+				jpsData.Add(new PosCompTime("PT", strLen, str));
+			}
+			if (test == "SI")  {
+				jpsData.Add(new SatIndex("SI", strLen, str));
+			}
+			if (test == "AN") {
+				jpsData.Add(new AntName("AN", strLen, str));
+			}
+			if (test == "NN") {
+				jpsData.Add(new SatNumbers("NN", strLen, str));
+			}
+			if (test == "EL") {
+				jpsData.Add(new SatElevations("EL", strLen, str, 1));
+			}
+			if (test == "AZ") {
+				 jpsData.Add(new SatAzimuth("AZ", strLen, str));
+			}
+			
+			if (test == "RC") {
+				 jpsData.Add(new PseudoRange("RC", strLen, str, "RC"));
+			}
+			if (test == "R1") {
+				 jpsData.Add(new PseudoRange("R1", strLen, str, "R1"));
+			}
+			if (test == "R2") {
+				 jpsData.Add(new PseudoRange("R2", strLen, str, "R2"));
+			}
+			if (test == "R3") {
+				 jpsData.Add(new PseudoRange("R3", strLen, str, "R3"));
+			}
+			if (test == "R5") {
+				 jpsData.Add(new PseudoRange("R5", strLen, str, "R5"));
+			}
+			if (test == "Rl") {
+				 jpsData.Add(new PseudoRange("Rl", strLen, str, "Rl"));
+			}
+			
+			if (test == "rx") {
+				 jpsData.Add(new InPseudoRanges("rx", strLen, str, "rx"));
+			}
+			if (test == "rc") {
+				 jpsData.Add(new InPseudoRanges("rc", strLen, str, "rc"));
+			}
+			if (test == "r1") {
+				 jpsData.Add(new InPseudoRanges("r1", strLen, str, "r1"));
+			}
+			if (test == "r2") {
+				 jpsData.Add(new InPseudoRanges("r2", strLen, str, "r2"));
+			}
+			if (test == "r3") {
+				 jpsData.Add(new InPseudoRanges("r3", strLen, str, "r3"));
+			}
+			if (test == "r5") {
+				 jpsData.Add(new InPseudoRanges("r5", strLen, str, "r5"));
+			}
+			if (test == "rl") {
+				 jpsData.Add(new InPseudoRanges("rl", strLen, str, "rl"));
+			}
+			if (test == "1R") {
+				 jpsData.Add(new RelPseudoRanges("1R", strLen, str, "1R"));
+			}
+			if (test == "2R") {
+				 jpsData.Add(new RelPseudoRanges("2R", strLen, str, "2R"));
+			}
+			if (test == "3R") {
+				 jpsData.Add(new RelPseudoRanges("3R", strLen, str, "3R"));
+			}
+			if (test == "5R") {
+				 jpsData.Add(new RelPseudoRanges("5R", strLen, str, "5R"));
+			}
+			if (test == "lR") {
+				 jpsData.Add(new RelPseudoRanges("lR", strLen, str, "lR"));
+			}
+			if (test == "cr") {
+				 jpsData.Add(new SRPR("cr", strLen, str, "cr"));
+			}
+			if (test == "1r") {
+				 jpsData.Add(new SRPR("1r", strLen, str, "1r"));
+			}
+			if (test == "2r") {
+				 jpsData.Add(new SRPR("2r", strLen, str, "2r"));
+			}
+			if (test == "3r") {
+				 jpsData.Add(new SRPR("3r", strLen, str, "3r"));
+			}
+			if (test == "5r") {
+				 jpsData.Add(new SRPR("5r", strLen, str, "5r"));
+			}
+			if (test == "lr") {
+				 jpsData.Add(new SRPR("lr", strLen, str, "lr"));
+			}
+			if (test == "CC") {
+				 jpsData.Add(new SC("CC", strLen, str, "CC"));
+			}
+			if (test == "C1") {
+				 jpsData.Add(new SC("C1", strLen, str, "C1"));
+			}
+			if (test == "C2") {
+				 jpsData.Add(new SC("C2", strLen, str, "C2"));
+			}
+			if (test == "C3") {
+				 jpsData.Add(new SC("C3", strLen, str, "C3"));
+			}
+			if (test == "C5") {
+				 jpsData.Add(new SC("C5", strLen, str, "C5"));
+			}
+			if (test == "Cl") {
+				 jpsData.Add(new SC("Cl", strLen, str, "Cl"));
+			}
+			if (test == "сс") {
+				 jpsData.Add(new SC("сс", strLen, str, "сс"));
+			}
+			if (test == "с1") {
+				 jpsData.Add(new SS("с1", strLen, str, "с1"));
+			}
+			if (test == "с2") {
+				 jpsData.Add(new SS("с2", strLen, str, "с2"));
+			}
+			if (test == "с3") {
+				 jpsData.Add(new SS("с3", strLen, str, "с3"));
+			}
+			if (test == "с5") {
+				 jpsData.Add(new SS("с5", strLen, str, "с5"));
+			}
+			if (test == "cl") {
+				 jpsData.Add(new SS("cl", strLen, str, "cl"));
+			}
+			if (test == "PC") {
+				 jpsData.Add(new CP("PC", strLen, str, "PC"));
+			}
+			if (test == "P1") {
+				 jpsData.Add(new CP("P1", strLen, str, "P1"));
+			}
+			if (test == "P2") {
+				 jpsData.Add(new CP("P2", strLen, str, "P2"));
+			}
+			if (test == "P3") {
+				 jpsData.Add(new CP("P3", strLen, str, "P3"));
+			}
+			if (test == "P5") {
+				 jpsData.Add(new CP("P5", strLen, str, "P5"));
+			}
+			if (test == "Pl") {
+				 jpsData.Add(new CP("Pl", strLen, str, "Pl"));
+			}
+						
+			if (test == "pc") {
+				 jpsData.Add(new SCP("pc", strLen, str, "pc"));
+			}
+			if (test == "p1") {
+				 jpsData.Add(new SCP("p1", strLen, str, "p1"));
+			}
+			if (test == "p2") {
+				 jpsData.Add(new SCP("p2", strLen, str, "p2"));
+			}
+			if (test == "p3") {
+				 jpsData.Add(new SCP("p3", strLen, str, "p3"));
+			}
+			if (test == "p5") {
+				 jpsData.Add(new SCP("p5", strLen, str, "p5"));
+			}
+			if (test == "pl") {
+				 jpsData.Add(new SCP("pl", strLen, str, "pl"));
+			}
+			
+			if (test == "CP") {
+				 jpsData.Add(new RCP_RC("CP", strLen, str, "CP"));
+			}
+			if (test == "1P") {
+				 jpsData.Add(new RCP_RC("1P", strLen, str, "1P"));
+			}
+			if (test == "2P") {
+				 jpsData.Add(new RCP_RC("2P", strLen, str, "2P"));
+			}
+			if (test == "3P") {
+				 jpsData.Add(new RCP_RC("3P", strLen, str, "3P"));
+			}
+			if (test == "5P") {
+				 jpsData.Add(new RCP_RC("5P", strLen, str, "5P"));
+			}
+			if (test == "lP") {
+				 jpsData.Add(new RCP_RC("lP", strLen, str, "lP"));
+			}
+			
+			if (test == "cp") {
+				 jpsData.Add(new RCP_rc("cp", strLen, str, "cp"));
+			}
+			if (test == "1p") {
+				 jpsData.Add(new RCP_rc("1p", strLen, str, "1p"));
+			}
+			if (test == "2p") {
+				jpsData.Add(new RCP_rc("2p", strLen, str, "2p"));
+			}
+			if (test == "3p") {
+				jpsData.Add(new RCP_rc("3p", strLen, str, "3p"));
+			}
+			if (test == "5p") {
+				jpsData.Add(new RCP_rc("5p", strLen, str, "5p"));
+			}
+			if (test == "lp") {
+				jpsData.Add(new RCP_rc("lp", strLen, str, "lp"));
+			}
+			
+			if (test == "DX") {
+				jpsData.Add(new Doppler("DX", strLen, str, "DX"));
+			}
+			if (test == "DC") {
+				jpsData.Add(new Doppler("DC", strLen, str, "DC"));
+			}
+			if (test == "D1") {
+				jpsData.Add(new Doppler("D1", strLen, str, "D1"));
+			}
+			if (test == "D2") {
+				jpsData.Add(new Doppler("D2", strLen, str, "D2"));
+			}
+			if (test == "D3") {
+				jpsData.Add(new Doppler("D3", strLen, str, "D3"));
+			}
+			if (test == "D5") {
+				jpsData.Add(new Doppler("D5", strLen, str, "D5"));
+			}
+			if (test == "Dl") {
+				jpsData.Add(new Doppler("Dl", strLen, str, "Dl"));
+			}
+			if (test == "0d") {
+				jpsData.Add(new SRDP("0d", strLen, str, "0d"));
+			}
+			if (test == "1d") {
+				jpsData.Add(new SRDP("1d", strLen, str, "1d"));
+			}
+			if (test == "2d") {
+				jpsData.Add(new SRDP("2d", strLen, str, "2d"));
+			}
+			if (test == "3d") {
+				jpsData.Add(new SRDP("3d", strLen, str, "3d"));
+			}
+			if (test == "5d") {
+				jpsData.Add(new SRDP("5d", strLen, str, "5d"));
+			}
+			if (test == "ld") {
+				jpsData.Add(new SRDP("ld", strLen, str, "ld"));
+			}
 		}
 		
 		
 		//Отладка
-		if (i > strLen + 100000)
-		{break;}
+		
+		if (i > strLen + 20000)
+		{LOG("FIN"); break;}
 	}
 	
 	//Закрытие (в данном случае файла)
@@ -143,45 +426,67 @@ void JpsFile::readFile()
 }
 
 
-//	ifstream inFileStr(name, ios::binary);
-//		inFileStr.read((char*)&x, sizeof(x));
-//	inFileStr.close();
-//	LOG("WTF output");
-//	RDUMP(x);
+void JpsFile::analyzeJpsVector()
+{
+	long int tod = 0;
+	ReceiverDate recDat;
+	Vector<int> intVect;
+	
+	for (int i = 0; i < jpsData.GetCount(); i++)
+	{
+		if ((jpsData[i] -> datType) == "SI"){
+			LOG("These are SI Value");
+			try{
+				SatIndex *SI = dynamic_cast<SatIndex*>(jpsData[i]);
+				//Записываем в большой массив нужыне нам номера
+			}catch(std::bad_cast){
+				LOG("SI Cast Failed");
+			}
+		}
+		
+		if ((jpsData[i] -> datType) == "NN"){
+			try{
+				SatNumbers *NN = dynamic_cast<SatNumbers*>(jpsData[i]);
+				//Записываем данные в массив
+				
+			}catch(std::bad_cast){
+				LOG("NN Cast Failed");
+			}
+		}
+		
+		if ((jpsData[i] -> datType) == "RT"){
+			try{
+				ReceiverTime *RT = dynamic_cast<ReceiverTime*>(jpsData[i]);
+				//Записываем данные в массив
+			}catch(std::bad_cast){
+				LOG("Failed RT Cast");
+			}
+		}
+		
+		if ((jpsData[i] -> datType) == "RD"){
+			try{
+				ReceiverDate *RD = dynamic_cast<ReceiverDate*>(jpsData[i]);
+				//Записываем данные в массив
+			}catch(std::bad_cast){
+				LOG("Failed RD Cast");
+			}
+		}
+		
+		if ((jpsData[i] -> datType) == "RC" || (jpsData[i] -> datType) == "R1"
+			|| (jpsData[i] -> datType) == "R2" || (jpsData[i] -> datType) == "R3"
+			|| (jpsData[i] -> datType) == "R5" || (jpsData[i] -> datType) == "Rl")
+		{
+			try{
+				PseudoRange *PR = dynamic_cast<PseudoRange*>(jpsData[i]);
+				//Записываем данные в массив
+			}catch(std::bad_cast){
+				LOG("Failed PR Cast");
+			}
+		}
+	}
+}
 
-
-//				if (jpsName == "JP") {JpsData* jps = new JpsFileId(jpsCont, jpsName, jpsBynStr); jpsData.Add(jps); delete jps;}
-//				if (jpsName == "MF") {JpsData* jps = new JpsFormat(jpsCont, jpsName, jpsBynStr); jpsData.Add(jps); delete jps;}
-//				if (jpsName == "~~") {
-//					JpsData* jps = new ReceiverTime(jpsName,jpsBynStr);
-//					jpsData.Add(jps),
-//					delete jps;
-//					LOG("strNum = " << AsString(i));
-//					LOG("Receiver Time Test");
-//					LOG(jpsCont);
-//					LOG(jpsBynStr);
-//					LOG(jpsString);
-//					LOG("");
-//				}
-//				if (jpsName == "::") {
-//					JpsData* jps = new EpochTime(jpsName, jpsBynStr);
-//					jpsData.Add(jps);
-//					delete jps;
-//					LOG("strNum = " << AsString(i));
-//					LOG("Epoch Time test");
-//					LOG(jpsCont);
-//					LOG(jpsBynStr);
-//					LOG(jpsString);
-//					LOG("");
-//				}
-//				if (jpsName == "RD") {
-//					JpsData* jps = new ReceiverDate(jpsName, jpsBynStr);
-//					jpsData.Add(jps);
-//					delete jps;
-//					LOG("strNum = " << AsString(i));
-//					LOG("Receiver Date Test");
-//					LOG(jpsCont);
-//					LOG(jpsBynStr);
-//					LOG(jpsString);
-//					LOG("");
-//				}
+void JpsFile::clearJpsDataVector()
+{
+	
+}
