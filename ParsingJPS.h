cb610945c3df67645a3b63cf067c5fadd13a56ff
vezviitiko <38458279+vezviitiko@users.noreply.github.com>
2018-08-28 16:37:25 +0300

    Update ParsingJPS.h

--- ParsingJPS.h
+++ ParsingJPS.h
@@ -3,10 +3,15 @@
 
 #include <Core/Core.h>
 #include <fstream>
+#include <math.h>
+#include <iostream>
 
 #include "JpsDataClass.h"
+#include "NavSol.h"
+#include "DataConverter.h"
 
 using namespace Upp;
+using namespace std;
 
 class JpsFile :public Moveable<JpsFile>{
 public:
@@ -15,11 +20,15 @@ public:
 	String content;
 	
 	String fileIdent;
+	//double JpsData;
 	
-	Vector <JpsData*> jpsData;
+	JpsData *jpsData;
+	//Vector <JpsData*> jpsData;
+	NavSolInputData navSolDat;
 	
 	void readFile();
 	void analyzeJpsVector();
+	void clearJpsDataVector();
 	
 	//Метод вывода на экран содержимого файла
 	void outputFile();
@@ -36,8 +45,26 @@ public:
 	void parse();
 };
 
+class TestJpsDataClass : public JpsData{
+	
+};
+
+class PseudoRangeData : public Moveable<PseudoRangeData>{
+public:
+	typedef PseudoRangeData CLASSNAME;
+	
+	double time;			//Эпоха измерений в форме MJD;
+	Vector <int> si;		//Номер аппарата в дальнейших сообщениях
+	Vector <int> nn;		//Индексы аппаратов в собственной системе
+	Vector <double> range;	//Значения псевдодальностей
+	String type;			//Тип псевдодальности (Возможно на что то повлияет)
+};
 
 //Дополнительные функции
 String Revert(String);
-String GetBitsStr(String recStr);
+String GetBitsStr(string recStr);
+double BinaryToDecimal(String binStr, String type);
+double HexToDec(String hexStr);
+String BinToHex(String binStr, int len);
+String BinaryStrGen(String binStr , int sizeBit, int start);
 #endif
