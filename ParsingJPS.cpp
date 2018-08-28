f3b5053fa1a17e1ed8cc53672c90963118fb6ee3
vezviitiko <38458279+vezviitiko@users.noreply.github.com>
2018-08-28 16:38:02 +0300

    Update ParsingJPS.cpp

--- ParsingJPS.cpp
+++ ParsingJPS.cpp
@@ -17,6 +17,19 @@ CONSOLE_APP_MAIN
 	for (int i = 0; i < jpsFile.GetCount(); i++){
 		LOG("I'm here");
 		jpsFile[i].readFile();
+		//jpsFile[i].analyzeJpsVector();
 		break;		//Убрать. Поставил для отладки
+		
 	}
+	
+	//очистка памяти
+	/*
+	for (int i = 0; i < jpsFile.GetCount(); i++){
+		for (int j = 0; j < jpsFile[i].jpsData.GetCount(); j++){
+			delete jpsFile[i].jpsData[j];
+		}
+	}*/
+	
+	
+	LOG("In the end of program");
 }
